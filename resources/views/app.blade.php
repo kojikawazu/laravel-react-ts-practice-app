6f299694334dde8d843cc6e326f36db34bbffebe
kojikawazu <nanairo7.daydream21@gmail.com>
2024-06-02 18:12:19 +0900

    デバッグ

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -11,7 +11,7 @@
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
         <!-- CSRFトークン -->
-        <meta name="csrf-token" content="{{ csrf_token() }}">
+        {{-- <meta name="csrf-token" content="{{ csrf_token() }}"> --}}
 
         <!-- Scripts -->
         @routes
