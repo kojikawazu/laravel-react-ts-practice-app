7007ed6be5d9afdb578a55d4a3fd9b785b9adf53
kojikawazu <nanairo7.daydream21@gmail.com>
2024-06-05 22:21:06 +0900

    バグ修正

--- app/Http/Controllers/MarkdownController.php
+++ app/Http/Controllers/MarkdownController.php
@@ -10,6 +10,9 @@ class MarkdownController extends Controller
 {
     public function index()
     {
+        Log::info('MarkdownController index start. ');
+
+        Log::info('MarkdownController index end. ');
         return inertia('Markdown/MarkdownPage', [
             'message' => session('message'),
         ]);
@@ -17,16 +20,21 @@ class MarkdownController extends Controller
 
     public function store(Request $request)
     {
+        Log::info('MarkdownController store start. ');
+
         $data = $request->validate([
             'content' => 'required',
         ]);
 
         try {
+            Log::info('MarkdownController store MarkdownPost::create() before. ');
+            
             $post = MarkdownPost::create([
                 'content' => $data['content'],
                 'user_id' => Auth::id(),
             ]);
     
+            Log::info('MarkdownController store end. ');
             return redirect()
                 ->route('markdown.index')
                 ->with('message', 'Post submitted successfully');
