77264c6c8920685e071d7d596b6989adbdc290f4
kojikawazu <nanairo7.daydream21@gmail.com>
2024-06-05 22:58:56 +0900

    バグ修正

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\URL;
+use Inertia\Inertia;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -20,16 +21,22 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        // httpをhttpsにリダイレクトする
         if (env('APP_ENV') !== 'local') {
             URL::forceScheme('https');
         }
 
+        // Inertiaの共有データ設定の追加
+        // この変更により、サーバーサイドで設定されたセッションメッセージやエラーメッセージがInertiaを介して
+        // フロントに渡される。
         Inertia::share([
+            // エラーメッセージ
             'errors' => function () {
                 return session()->get('errors')
                     ? session()->get('errors')->getBag('default')->getMessages()
                     : (object) [];
             },
+            // セッションメッセージ
             'message' => function () {
                 return session()->get('message') ? session()->get('message') : null;
             },
