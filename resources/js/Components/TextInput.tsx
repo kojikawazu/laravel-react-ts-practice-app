982dd609f88df223f44583c12f5f89de7c3a3635
kojikawazu <nanairo7.daydream21@gmail.com>
2024-07-05 22:04:29 +0900

    プロフィール画面のスタイル調整

--- resources/js/Components/TextInput.tsx
+++ resources/js/Components/TextInput.tsx
@@ -21,7 +21,7 @@ export default forwardRef(function TextInput(
             {...props}
             type={type}
             className={
-                'border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm ' +
+                'border-amber-300 focus:border-amber-500 focus:ring-amber-500 rounded-md shadow-sm ' +
                 className
             }
             ref={localRef}
